0f3a99ab343c45b56ff38ae1ad06cfb00d5226d3
SIMON-HP\simon <simonphindley@gmail.com>
2023-03-05 16:52:16 +0000

    V0.005

--- grader_logic.py
+++ grader_logic.py
@@ -1,5 +1,5 @@
 import json
-import re
+import re, os
 
 class ExamGrader:
     def __init__(self, exam_folder_path, criteria_file_path):
@@ -16,28 +16,38 @@ class ExamGrader:
             self.criteria = json.load(f)
 
     def grade_exam(self):
-        exam_files = ['R1config.txt', 'S1config.txt']
+        exam_files = [f for f in os.listdir(self.exam_folder_path) if f.endswith('.txt')]
 
         self.results_dict = {}
         for exam_file in exam_files:
-            with open(f"{self.exam_folder_path}/{exam_file}", 'r') as f:
+            with open(os.path.join(self.exam_folder_path, exam_file), 'r') as f:
                 exam_file_contents = f.read()
 
             print(f'Grading {exam_file}...')
             self.results_dict[exam_file] = {}
+
+            # Check if this is an R1 or S1 file
+            r_or_s = ''
+            if exam_file.startswith('R1'):
+                r_or_s = 'R1'
+            elif exam_file.startswith('S1'):
+                r_or_s = 'S1'
+
             for key in self.criteria.keys():
-                if self.criteria[key]['regex']:
-                    if re.search(self.criteria[key]['answer'], exam_file_contents, re.IGNORECASE):
-                        self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': self.criteria[key]['weight']}
-                        self.total_score += self.criteria[key]['weight']
+                # Only grade against the criteria that belong to this file
+                if key.endswith(r_or_s) or not key.endswith('config.txt'):
+                    if self.criteria[key]['regex']:
+                        if re.search(self.criteria[key]['answer'], exam_file_contents, re.IGNORECASE):
+                            self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': self.criteria[key]['weight']}
+                            self.total_score += self.criteria[key]['weight']
+                        else:
+                            self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': 0}
                     else:
-                        self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': 0}
-                else:
-                    if self.criteria[key]['answer'].lower() in exam_file_contents.lower():
-                        self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': self.criteria[key]['weight']}
-                        self.total_score += self.criteria[key]['weight']
-                    else:
-                        self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': 0}
+                        if self.criteria[key]['answer'].lower() in exam_file_contents.lower():
+                            self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': self.criteria[key]['weight']}
+                            self.total_score += self.criteria[key]['weight']
+                        else:
+                            self.results_dict[exam_file][key] = {'answer': self.criteria[key]['answer'], 'weight': self.criteria[key]['weight'], 'points': 0}
 
     def get_results_dict(self):
         return self.results_dict
